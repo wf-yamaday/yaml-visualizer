cac10cce1e93a9bd229eff0639ec16b9f6cc5beb
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-07-11 00:50:23 +0900

    feat: cli tool

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+install_requires = [
+    'PyYAML >= 3.10, < 6',
+    'graphviz'
+]
+
+setup(
+    name='yaml-visualizer',
+    description='visualize resources defined by docker-compose.yml.',
+    author='wf-yamaday',
+    version='0.0.1',
+    packages=['src'],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'yaml-vis=src.cli.main:main'
+        ],
+    },
+)
